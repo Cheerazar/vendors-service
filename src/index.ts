722a0cd470d8be805400c75ab8b6876ba9db0ca6
Gunnari Auvinen <gauvinen@gmail.com>
2020-11-14 17:31:00 -0500

    chore: Add body-parser middleware.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const { PORT_NUMBER = 3000 } = process.env;
 
+app.use(bodyParser.json());
 app.get('/');
 
 app.listen(PORT_NUMBER, () => console.log(`Listening on PORT: ${PORT_NUMBER}`));
